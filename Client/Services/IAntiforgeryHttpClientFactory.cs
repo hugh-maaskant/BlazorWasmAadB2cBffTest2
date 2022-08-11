bf508dd4e9eb26e0db64788b3cc9ed48df01424b
Hugh Maaskant <hugh.maaskant@outlook.com>
2022-08-11 17:58:40 +0200

    Initial commit - as created from the 1.2.1 template see https://github.com/damienbod/Blazor.BFF.AzureB2C.Template

---
+++ Client/Services/IAntiforgeryHttpClientFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorWasmAadB2cBffTest2.Client.Services;
+
+public interface IAntiforgeryHttpClientFactory
+{
+    Task<HttpClient> CreateClientAsync(string clientName = "authorizedClient");
+}
---
+++ Server/Program.cs
@@ -0,0 +1,103 @@
+﻿using BlazorWasmAadB2cBffTest2.Server;
+using BlazorWasmAadB2cBffTest2.Server.Services;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.AddServerHeader = false;
+});
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+var env = builder.Environment;
+IServiceProvider? applicationServices = null;
+
+services.AddScoped<MsGraphService>();
+services.AddScoped<MsGraphClaimsTransformation>();
+services.AddScoped<CaeClaimsChallengeService>();
+
+services.AddAntiforgery(options =>
+{
+    options.HeaderName = "X-XSRF-TOKEN";
+    options.Cookie.Name = "__Host-X-XSRF-TOKEN";
+    options.Cookie.SameSite = SameSiteMode.Strict;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
+
+services.AddHttpClient();
+services.AddOptions();
+
+//var scopes = Configuration.GetValue<string>("DownstreamApi:Scopes");
+//string[] initialScopes = scopes.Split(' ');
+
+services.AddMicrosoftIdentityWebAppAuthentication(configuration, "AzureB2C")
+    .EnableTokenAcquisitionToCallDownstreamApi(Array.Empty<string>())
+    .AddInMemoryTokenCaches();
+
+services.Configure<MicrosoftIdentityOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.Events.OnTokenValidated = async context =>
+    {
+        if (applicationServices != null && context.Principal != null)
+        {
+            using var scope = applicationServices.CreateScope();
+            context.Principal = await scope.ServiceProvider
+                .GetRequiredService<MsGraphClaimsTransformation>()
+                .TransformAsync(context.Principal);
+        }
+    };
+});
+
+services.AddControllersWithViews(options =>
+    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+
+services.AddRazorPages().AddMvcOptions(options =>
+{
+    //var policy = new AuthorizationPolicyBuilder()
+    //    .RequireAuthenticatedUser()
+    //    .Build();
+    //options.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+applicationServices = app.Services;
+
+if (env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseSecurityHeaders(
+        SecurityHeadersDefinitions.GetHeaderPolicyCollection(env.IsDevelopment(),
+            configuration["AzureB2C:Instance"]));
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseNoUnauthorizedRedirect("/api");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+    endpoints.MapNotFound("/api/{**segment}");
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+app.Run();
